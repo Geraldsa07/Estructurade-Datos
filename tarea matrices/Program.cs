44a61eea6dff8cc202034b65b1747c104fce5022
Gerald Santamaria <126569575+Geraldsa07@users.noreply.github.com>
2023-10-05 11:48:07 -0600

    Program.cs

--- tarea matrices/Program.cs
+++ tarea matrices/Program.cs
@@ -1,15 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
 
 namespace tarea_matrices
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            Console.Write("Ingrese el tamaño de la matriz cuadrada: ");
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] matriz = new int[n, n];
+
+            Console.WriteLine("Ingrese los elementos de la matriz:");
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write($"Elemento [{i}, {j}]: ");
+                    matriz[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine("Elementos en la diagonal principal:");
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(matriz[i, i] + " ");
+            }
+
+            Console.WriteLine("\n Elementos en la diagonal secundaria:");
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(matriz[i, n - 1 - i] + " ");
+            }
+
+            Console.ReadLine();
         }
     }
 }
